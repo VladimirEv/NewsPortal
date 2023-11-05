e8ba5d4359717eb110a314415a136f4d8c0c3fce
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-11-05 16:29:17 +0300

    feat: added new class RoleClaimEntityConfiguration.          This class realise method Configure and we got name           for database table "RoleClaims"

---
+++ NewsPortal.Persistance/Configuration/RoleClaimEntityConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NewsPortal.Domain.Entities;
+
+namespace NewsPortal.Persistance.Configuration
+{
+    ///<summary>
+    ///IEntityTypeConfiguration<RoleClaim> - интерфейс используется для настройки сущностей
+    ///в Entity Framework Core
+    ///</summary>
+
+    public class RoleClaimEntityConfiguration : IEntityTypeConfiguration<RoleClaim>
+    {
+        /// <summary> Работа метода Configure.
+        ///Внутри этого метода выполняются настройки сущности RoleClaim.
+        ///устанавливается имя таблицы, в которой будут храниться данные сущности RoleClaim
+        ///Строка builder.ToTable("RoleClaims") указывает, что сущность RoleClaimEntity будет
+        ///отображаться в таблице с именем "RoleClaims" в базе данных
+        /// </summary>
+        
+        public void Configure(EntityTypeBuilder<RoleClaim> builder)
+        {
+            builder.ToTable("RoleClaims");
+        }
+    }
+}
