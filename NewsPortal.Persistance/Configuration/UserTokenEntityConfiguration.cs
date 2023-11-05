ac896bb7cce7b8a5e8ff23b293ad497f39a59fe9
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-11-05 16:47:54 +0300

    feat: added new class UserTokenEntityConfiguration BREAKING CHANGE:  This class realise method Configure and we got name                                        for database table "UserTokens"

---
+++ NewsPortal.Persistance/Configuration/UserTokenEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NewsPortal.Domain.Entities;
+
+namespace NewsPortal.Persistance.Configuration
+{
+    public class UserTokenEntityConfiguration : IEntityTypeConfiguration<UserToken>
+    {       
+        public void Configure(EntityTypeBuilder<UserToken> builder)
+        {
+            builder.ToTable("UserTokens");
+        }
+    }
+}
