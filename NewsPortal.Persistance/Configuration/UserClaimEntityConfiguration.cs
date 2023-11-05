1cbef8fac529d14b35ab76b0c75c6129df714347
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-11-05 16:42:52 +0300

    feat: added new classes UserClaimEntityConfiguration and UserEntityConfiguration
    BREAKING CHANGE:  This class realise methods Configure and we got names
                                           for database tables "UserClaims" and "Users"

---
+++ NewsPortal.Persistance/Configuration/UserClaimEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NewsPortal.Domain.Entities;
+
+namespace NewsPortal.Persistance.Configuration
+{
+    public class UserClaimEntityConfiguration : IEntityTypeConfiguration<UserClaim>
+    {       
+        public void Configure(EntityTypeBuilder<UserClaim> builder)
+        {
+            builder.ToTable("UserClaims");
+        }
+    }
+}
