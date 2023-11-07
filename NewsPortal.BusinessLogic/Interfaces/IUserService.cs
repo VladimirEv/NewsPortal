7d1f31b2648508fd1d53fc89bf5692f4c9cee9b6
Vladimir Belevich <vladimir.ev.belevich@gmail.com>
2023-11-07 13:43:37 +0300

    fix: added new namespace for UserModel

--- NewsPortal.BusinessLogic/Interfaces/IUserService.cs
+++ NewsPortal.BusinessLogic/Interfaces/IUserService.cs
@@ -1,7 +1,22 @@
-﻿namespace NewsPortal.BusinessLogic.Interfaces
+﻿using NewsPortal.BusinessLogic.Models;
+using NewsPortal.BusinessLogic.Models.RequestModels;
+using NewsPortal.Domain.Entities;
+using NewsPortal.Infrastucture.Common.Response.ResponseModels.Base;
+
+namespace NewsPortal.BusinessLogic.Interfaces
 {
     public interface IUserService
     {
-
+        Task<BaseResponse> Register(RegisterModel registerModel);
+        Task<BaseResponse> UpdateUserById(Guid userId, UserModel model);
+        Task<BaseResponse> ChangeEmailById(Guid userId, string newEmail, string token);
+        Task<BaseResponse> ChangePasswordById(Guid userId, ChangePasswordModel model);
+        Task<BaseResponse> GenerateChangeEmailTokenById(Guid userId, string newEmail);
+        Task<BaseResponse> BlockUserById(Guid userId, int hours = 24);
+        Task<BaseResponse> GetAll();
+        Task<BaseResponse> GetById(Guid userId);
+        Task<BaseResponse> GetByEmail(string email);
+        Task<User?> GetUserByEmail(string email);
+        Task<User?> GetUserById(Guid userId);
     }
 }
